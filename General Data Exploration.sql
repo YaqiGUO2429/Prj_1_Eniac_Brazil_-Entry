d61d7df3db326734917be31181fe6d1f4bbd38cf
YaqiGUO2429 <yqg32996@gmail.com>
2025-05-09 11:42:50 +0200

    Update General Data Exploration.sql
    Fixed some DISTINCT misuse: 
    
     `SELECT (DISTINCT column1)`        ❌ Not valid SQL syntax 
                        
     `DISTINCT COUNT(column1)`         ❌ Misleading: `DISTINCT` does nothing here
    
     `SELECT column1, DISTINCT column2`   ❌ Cannot use `DISTINCT` between columns
    
    ✅1. Basic (row-level) SELECT DISTINCT col1, col2 FROM table; Returns all unique combinations of col1 and col2  
    
    ✅2. Single column SELECT DISTINCT col1 FROM table; 
    Returns all unique values from col1  
    
    ✅3. Multiple columns SELECT DISTINCT col1, col2 FROM table; Removes duplicate rows where both col1 and col2 are the same  
    
    ✅4. With aggregation SELECT COUNT(DISTINCT col1) FROM table; Counts the number of unique (non-null) values in col1

--- General Data Exploration.sql
+++ General Data Exploration.sql
@@ -1,10 +1,13 @@
 USE magist;
 
 #1. How many orders are there in the dataset?
-SELECT count( DISTINCT order_id) AS order_amount FROM orders;
+SELECT count(DISTINCT order_id) 
+    AS order_amount 
+FROM orders;
 
 #2. Are orders actually delivered?
-SELECT DISTINCT order_status, count(DISTINCT order_id) AS orders_amount
+SELECT order_status, 
+       count(DISTINCT order_id) AS orders_amount
 FROM orders
 GROUP BY order_status
 ORDER BY orders_amount DESC;
@@ -14,7 +17,7 @@ ORDER BY orders_amount DESC;
 SELECT 
     YEAR(order_purchase_timestamp) AS order_year,
     MONTH(order_purchase_timestamp) AS order_month,
-    count(customer_id) AS cunstomer_amount
+    count(DISTINCT customer_id) AS cunstomer_amount
 FROM
     orders
 GROUP BY order_year, order_month
@@ -22,23 +25,26 @@ ORDER BY order_year, order_month ASC;
 #year level
 SELECT 
     YEAR(order_purchase_timestamp) AS order_year,
-    count(customer_id) AS cunstomer_amount
+    count(DISTINCT customer_id) AS cunstomer_amount
 FROM
     orders
 GROUP BY order_year
 ORDER BY order_year ASC;
 
 #4. How many products are there in the products table?
-SELECT count(DISTINCT product_id) AS products_count FROM products;
+SELECT count(DISTINCT product_id) 
+    AS products_count 
+FROM products;
 
 #5. Which are the categories with most products?
-SELECT DISTINCT p.product_category_name, pcnt.product_category_name_english,
-count(DISTINCT p.product_id) AS products_amount
+SELECT p.product_category_name), 
+       pcnt.product_category_name_english,
+       count(DISTINCT p.product_id) AS products_amount
 FROM products p
 LEFT JOIN product_category_name_translation pcnt
 USING (product_category_name)
-GROUP BY product_category_name
-ORDER BY count(DISTINCT p.product_id) DESC;
+GROUP BY product_category_name, pcnt.product_category_name_english
+ORDER BY product_amount DESC;
 
 #6. How many of those products were present in actual transactions?
 SELECT COUNT(DISTINCT product_id) AS used_product_count
@@ -47,22 +53,24 @@ WHERE product_id IS NOT NULL;
 
 #7. What’s the price for the most expensive and cheapest products?
 # most expensive product
-SELECT DISTINCT(product_id), price
+SELECT DISTINCT product_id, 
+       price
 FROM order_items
 WHERE price = (SELECT MAX(price) FROM order_items);
 # most cheap product
-SELECT DISTINCT(product_id), price
+SELECT DISTINCT product_id, 
+       price
 FROM order_items
 WHERE price = (SELECT MIN(price) FROM order_items);
 
 # 8. What are the highest and lowest payment values?
 # highest payment value
-SELECT DISTINCT MAX(payment_value) 
+SELECT MAX(payment-value)
+    AS highest_payment
 FROM order_payments 
-AS highest_payment
 WHERE payment_value IS NOT NULL;
 # lowest payment value
-SELECT DISTINCT MIN(payment_value) 
+SELECT MIN(payment_value)
+    AS lowest_payment
 FROM order_payments 
-AS lowest_payment
 WHERE payment_value IS NOT NULL;
